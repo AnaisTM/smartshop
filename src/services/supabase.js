83ded1e1a9d9c3316f648281cf909eae80f6fe1c
AnaisTM <anaisalejandratorresmorales@gmail.com>
2026-01-02 22:13:16 -0300

    test

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://iaiiymtupxwklvmmneuw.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhaWl5bXR1cHh3a2x2bW1uZXV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNjg1NDIsImV4cCI6MjA4Mjk0NDU0Mn0.Hdv_EOYki7_jVH9wXpwRg72_JSGPujhEBKoMY6OhkWA'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 
 export const supabase = createClient(
   supabaseUrl,
